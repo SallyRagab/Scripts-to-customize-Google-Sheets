1e24a1107d01135642f00ba9bbf5c1ae04e88089
Sally Ragab <sallyragab31@gmail.com>
2018-08-29 11:32:47 +0200

    Update scriptsV3.js

--- scriptsV3.js
+++ scriptsV3.js
@@ -41,8 +41,8 @@ function getInputFromBox() {
 
 function respondToOK(e) {
   var app = UiApp.getActiveApplication();
-  var result1 = e.parameter.tBox1.replace(/ /g,"-").toLowerCase();
-  var result2 = e.parameter.tBox2.replace(/ /g,"-").toLowerCase();
+  var result1 = e.parameter.tBox1.replace(/ /g,"-").replace(/\$/g,"").replace(/\+/g,"").toLowerCase();
+  var result2 = e.parameter.tBox2.replace(/ /g,"-").replace(/\$/g,"").replace(/\+/g,"").toLowerCase();
   
   //Enter the result in the cell
   var sh0 = SpreadsheetApp.getActiveSpreadsheet().getSheets()[0];
@@ -60,26 +60,23 @@ function respondToOK(e) {
 }
 
 //DDM convert
-function getInputFromBox() {
-  var ss = SpreadsheetApp.getActiveSpreadsheet();
+function getInput() {
+  var s2 = SpreadsheetApp.getActiveSpreadsheet();
   //Create the user interface
-  var app = UiApp.createApplication()
+  var app2 = UiApp.createApplication()
     .setWidth(300)
     .setHeight(100);
   //Create one textboxs
-  var tBox1 = app.createTextBox().setName('tBox1').setId('tBox1');
-  var tBox2 = app.createTextBox().setName('tBox2').setId('tBox2');
+  var tBox12 = app.createTextBox().setName('tBox1').setId('tBox1');
   //Create the OK button and a handler to respond to when OK is clicked.
-  var okHandler = app.createServerHandler('respondToOK');
-  var btnOK = app.createButton('OK', okHandler)
+  var okHandler2 = app.createServerHandler('respondToOK');
+  var btnOK2 = app.createButton('OK', okHandler)
       .setWidth(60)
       .setHeight(20);;
   //I find grids very handy for arranging labels and widgets like textboxes neatly
-  var myGrid = app.createGrid(3, 2).setId('myGrid')
+  var myGrid2 = app.createGrid(3, 2).setId('myGrid2')
     .setText(0, 0, 'HO ID:')
     .setWidget(0, 1, tBox1)
-    .setText(1, 0, 'Plan Name:')
-    .setWidget(1, 1, tBox2)
     .setWidget(2, 1, btnOK)
     .setWidth(300)
     .setHeight(100);
@@ -88,27 +85,20 @@ function getInputFromBox() {
     You could add multiple callback elements to each of the textboxes but that's just extra code.
     As all the textboxes are added to myGrid then just one callback to that will work fine.
   */
-  okHandler.addCallbackElement(myGrid);
-  app.add(myGrid);
-  ss.show(app);
+  okHandler.addCallbackElement(myGrid2);
+  app.add(myGrid2);
+  s2.show(app2);
 }
 
-function respondToOK(e) {
-  var app = UiApp.getActiveApplication();
-  var result1 = e.parameter.tBox1.replace(/ /g,"-").toLowerCase();
-  var result2 = e.parameter.tBox2.replace(/ /g,"-").toLowerCase();
+function respondTo(e) {
+  var app2 = UiApp.getActiveApplication();
+  var result = e.parameter.tBox1.replace(/ /g,";").toLowerCase();
   
   //Enter the result in the cell
-  var sh0 = SpreadsheetApp.getActiveSpreadsheet().getSheets()[0];
-  var cellAddress = sh0.getActiveCell().getA1Notation();
-  sh0.getRange("D5").setValue(result1 + "&aff_sub=FMP-" + result2 + "-%%SOURCE%%");
-  sh0.getRange("D8").setValue(result2 + "-secure");
-  
-  var sh1 = SpreadsheetApp.getActiveSpreadsheet().getSheets()[1];
-  var cellAddress2 = sh1.getActiveCell().getA1Notation();
-  sh1.getRange("D5").setValue(result1 + "&aff_sub=FBB-" + result2  + "-%%SOURCE%%");
-  sh1.getRange("D8").setValue(result2 + "-secure");
+  var sh = SpreadsheetApp.getActiveSpreadsheet().getSheets()[0];
+  var cellAddress4 = sh.getActiveCell().getA1Notation();
+  sh.getRange("D5").setValue(result);
   
  //Close the user interface
-  return app.close();
+  return app2.close();
 }
